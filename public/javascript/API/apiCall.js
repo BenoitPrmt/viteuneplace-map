67ed3a7f9439d0f9c0973493c52321823391102e
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-19 20:15:08 +0100

    changed api endpoint to have live data

--- public/javascript/API/apiCall.js
+++ public/javascript/API/apiCall.js
@@ -5,7 +5,7 @@ async function getParking() {
         Accept: "application/json",
     };
     try {
-        const response = await fetch("https://data.orleans-metropole.fr/api/explore/v2.1/catalog/datasets/mobilite-places-disponibles-parkings-en-temps-reel/records?limit=100", {
+        const response = await fetch("https://data.orleans-metropole.fr/api/explore/v2.1/catalog/datasets/om-mobilite-parcs-stationnement/records?limit=100", {
             method: "GET",
             headers: headers,
         });
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -33,21 +33,39 @@ var redIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
+var greyIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-grey.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 function buildMarkers() {
     getParking().then((data) => {
         data.results.forEach((parking) => {
-            let coords = parking.coords.split(",");
+            // let coords = parking.coords.split(",");
 
             var marker;
-            if (parseInt(parking.dispo) === 0) {
-                marker = L.marker([coords[0], coords[1]], {icon: redIcon}).addTo(map);
-            } else if (parseInt(parking.dispo) > 0 && parseInt(parking.dispo) < 30) {
-                marker = L.marker([coords[0], coords[1]], {icon: goldIcon}).addTo(map);
+            if (parseInt(parking.nb_places_disponibles) === 0) {
+                marker = L.marker([parking.ylat, parking.xlong], {icon: redIcon}).addTo(map);
+            } else if (parseInt(parking.nb_places_disponibles) > 0 && parseInt(parking.nb_places_disponibles) < 30) {
+                marker = L.marker([parking.ylat, parking.xlong], {icon: goldIcon}).addTo(map);
+            } else if (parking.places_disponibles_temps_reel !== "oui") {
+                marker = L.marker([parking.ylat, parking.xlong], {icon: greyIcon}).addTo(map);
             } else {
-                marker = L.marker([coords[0], coords[1]], {icon: greenIcon}).addTo(map);
+                marker = L.marker([parking.ylat, parking.xlong], {icon: greenIcon}).addTo(map);
             }
 
-            marker.bindPopup(`<b>${parking.name}</b><br>Place(s) disponible(s) : ${parking.dispo}<br>Place(s) totale(s) : ${parking.total}`).openPopup()
+            let popupText;
+            if (parking.places_disponibles_temps_reel === "oui") {
+                popupText = `<b>${parking.nom}</b><br>Place(s) disponible(s) : ${parking.nb_places_disponibles}<br>Place(s) totale(s) : ${parking.nb_places}<br>Disponibilité : ${Math.floor(parking.taux_disponibilite)}%`;
+            } else {
+                popupText = `<b>${parking.nom}</b><br>Place(s) disponible(s) : Aucune donnée<br>Place(s) totale(s) : ${parking.nb_places}`;
+            }
+
+            marker.bindPopup(popupText).openPopup()
         });
     });
 }
