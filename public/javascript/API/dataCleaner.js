9a526d104e576b4273e8354e83406461f16259b4
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-22 23:46:50 +0100

    added multiple cities

--- public/javascript/API/dataCleaner.js
+++ public/javascript/API/dataCleaner.js
@@ -1,39 +1,20 @@
 function cleanData(data, city) {
-  /*
-  * Nettoyage des datas de chaque parking
-  * Chaque ville a des données différentes, il faut donc adapter ces données pour les rendre utilisables sur la carte
-  *
-  * Arguments:
-  * - data: les données du parking (array d'objets)
-  * - city: la ville du parking (string)
-  *
-  * Retour:
-  * - dataCleaneds: les données du parking nettoyées (array d'objets)
-  * */
+    /*
+    * Nettoyage des datas de chaque parking
+    * Chaque ville a des données différentes, il faut donc adapter ces données pour les rendre utilisables sur la carte
+    *
+    * Arguments:
+    * - data: les données du parking (array d'objets)
+    * - city: la ville du parking (string)
+    *
+    * Retour:
+    * - dataCleaneds: les données du parking nettoyées (array d'objets)
+    * */
 
     let dataCleaneds = [];
 
     switch (city) {
-        case "AixMarseille": // A VERIFIER
-
-            // "nom":"Centre Bourse",
-            // "voitureplacescapacite":1579,
-            // "voitureplacesdisponibles":1247,
-            // "description":null,
-            // "typeparking":"Parking",
-            // "site":null,
-            // "commune":"MARSEILLE",
-            // "adresse":"Rue Reine Elisabeth",
-            // "codepostal":"13015",
-            // "tempsreel":"True",
-            // "voitureplacesoccupees":332,
-            // "producteur":null,
-            // "longitude":5.3746211528778,
-            // "latitude":43.296632284718,
-            // "pointgeo":{
-            // "lon":5.3746211528778,
-            //     "lat":43.296632284718
-
+        case "AixMarseille":
             data.results.forEach((parking) => {
                 dataCleaneds.push({
                     name: parking.nom,
@@ -46,7 +27,19 @@ function cleanData(data, city) {
                 });
             });
             break;
-
+        case "Orleans":
+            data.results.forEach((parking) => {
+                dataCleaneds.push({
+                    name: parking.nom,
+                    xlong: parking.xlong,
+                    ylat: parking.ylat,
+                    realtime: parking.places_disponibles_temps_reel === "oui",
+                    available: parking.nb_places_disponibles,
+                    total: parking.nb_places,
+                    disponibility: parking.taux_disponibilite,
+                });
+            });
+            break;
     }
     return dataCleaneds;
 }
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([DEFAULT_MAP_CENTER.lat, DEFAULT_MAP_CENTER.long], 12);
+let map = L.map('map').setView([DEFAULT_MAP_CENTER.lat, DEFAULT_MAP_CENTER.long], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     // maxZoom: 20,
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var greenIcon = new L.Icon({
+let greenIcon = new L.Icon({
     iconUrl: 'img/marker-icon-green.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
@@ -15,7 +15,7 @@ var greenIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-var goldIcon = new L.Icon({
+let goldIcon = new L.Icon({
     iconUrl: 'img/marker-icon-2x-gold.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
@@ -24,7 +24,7 @@ var goldIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-var redIcon = new L.Icon({
+let redIcon = new L.Icon({
     iconUrl: 'img/marker-icon-2x-red.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
@@ -33,7 +33,7 @@ var redIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-var greyIcon = new L.Icon({
+let greyIcon = new L.Icon({
     iconUrl: 'img/marker-icon-2x-grey.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
@@ -42,40 +42,24 @@ var greyIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-function buildMarkers() {
-    getParking("https://data.orleans-metropole.fr/api/explore/v2.1/catalog/datasets/om-mobilite-parcs-stationnement/records?limit=100").then((data) => {
-        data.results.forEach((parking) => {
-            // let coords = parking.coords.split(",");
-
-            var marker;
-            if (parseInt(parking.nb_places_disponibles) < 2) {
-                marker = L.marker([parking.ylat, parking.xlong], {icon: redIcon}).addTo(map);
-            } else if (parseInt(parking.taux_disponibilite) >= 2 && parseInt(parking.taux_disponibilite) < 20) {
-                marker = L.marker([parking.ylat, parking.xlong], {icon: goldIcon}).addTo(map);
-            } else if (parking.places_disponibles_temps_reel !== "oui") {
-                marker = L.marker([parking.ylat, parking.xlong], {icon: greyIcon}).addTo(map);
-            } else {
-                marker = L.marker([parking.ylat, parking.xlong], {icon: greenIcon}).addTo(map);
-            }
-
-            let popupText;
-            if (parking.places_disponibles_temps_reel === "oui") {
-                popupText = `<b>${parking.nom}</b><br>Place(s) disponible(s) : ${parking.nb_places_disponibles}<br>Place(s) totale(s) : ${parking.nb_places}<br>Disponibilité : ${Math.floor(parking.taux_disponibilite)}%`;
-            } else {
-                popupText = `<b>${parking.nom}</b><br>Place(s) disponible(s) : Aucune donnée<br>Place(s) totale(s) : ${parking.nb_places}`;
-            }
-
-            marker.bindPopup(popupText).openPopup()
-        });
+fetch("cities.json").then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+        for (const d in data) {
+            console.log(d)
+            console.log(data[d])
+            buildMarkers(data[d], d)
+        }
     });
+})
 
-    getParking("https://data.ampmetropole.fr/api/explore/v2.1/catalog/datasets/disponibilites-des-places-de-parkings/records?limit=100").then((data) => {
-        data = cleanData(data, "AixMarseille")
+function buildMarkers(url, city) {
+    getParking(url).then((data) => {
+        data = cleanData(data, city)
         console.log(data)
         data.forEach((parking) => {
-            // let coords = parking.coords.split(",");
 
-            var marker;
+            let marker;
             if (!parking.realtime) {
                 marker = L.marker([parking.ylat, parking.xlong], {icon: greyIcon}).addTo(map);
             } else {
